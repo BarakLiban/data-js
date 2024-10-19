10259cbf5216311d1cf3bbcc943de4efc1477ef6
barakaliban <mubarakkalla5@gmail.com>
2024-10-19 12:48:07 +0300

    version 2

--- data-types.js
+++ data-types.js
@@ -1,1 +1,47 @@
+// used var keyword to define variables
 var name;
+var gender= true;// boolean --- true or false
+var age=60;
+var height=3;
+var isBald=false;
+var hasBeare=true;
+var wearSpecs=false;
+
+// use let to define variables
+let yearBorn=1880;
+let mothersName="fatuma";
+let fathersName="kala";
+
+const schoolName="inten"
+const villageName="isiolo"
+
+name="john"
+
+schoolName="alliance"
+name="mubarak"
+
+// data types in js
+// 1. number
+const age=20;
+// 2. string
+mothersName="fatume";
+// 3.boolean
+const isMale=true;
+// 4.object
+const persson={
+    name:'john',
+    age:20,
+    isMale:true
+};
+// 5. array
+const names=["mubarak","liban"];
+const isFemale=[true,false,false];
+const phoneNumber=[1,2,3,4];
+// 6 null 
+const newPerson=null;
+
+// 7. undefined
+let car;
+
+
+
---
+++ topics/i-o.js
@@ -0,0 +1,10 @@
+// input and output in js
+// 1. Alert
+// alert 
+// alert("password must contain atleast one lowercase and symbols");
+// 2 prompt
+// const name= prompt('who are you?');
+// 3. confirm
+// const isAdult= confirm("true or false");
+// 4. console.log
+console.log('hello world');
