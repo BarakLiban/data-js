3a757f2ce016eb0539753a4ad94015623db824c0
BarakLiban <mubarakkalla5@gmail.com>
2024-10-26 14:08:41 +0300

    Add files via upload

--- topics/arithemetic.js
+++ topics/arithemetic.js
@@ -1,1 +1,74 @@
+console.log(5+5);// the memory calculates and processes the output as 10
 
+// create variables
+
+let a = 5;
+let b = 5;
+
+console.log(a+b);// 10
+
+// allow user to give the output
+
+
+
+// define intergers which are 8
+let num1=parseInt(prompt("Enter the first number,24"));
+let num2=parseInt(prompt("Enter the second number,4"));
+let num3=parseInt(prompt("Enter the third number,100"));
+let num4=parseInt(prompt("Enter the fourth number,10"));
+let num5=parseInt(prompt("Enter the fifth number,24"));
+let num6=parseInt(prompt("Enter the sixth number,400"));
+let num7=parseInt(prompt("Enter the seventh number,200"));
+let num8=parseInt(prompt("Enter the eight number,2"));
+
+
+console.log(num1 * num2/num3+num4-num5+num6-num7/num8);
+
+
+// types of operators
+//1. Arithmetic operators
+1+2+3+4/5-6
+// 2.unary operators  --means 1.++, --
+
+let x = 5;
+x++
+
+console.log(x);// 6
+
+// 3.binary operators -- 2 operands
+let y = 5;
+let z = 6;
+
+
+// 4.logical operators
+
+// logical OR ||
+
+
+//condition
+if (x > 5 || y > 5) {
+    console.log("x or y is greater than 5");
+}
+
+// logical AND
+
+if (x > 5 && y > 5) {
+  console.log("x and y is greater than 5");
+}
+
+
+
+//FOR NEXT SESSION
+
+// use loop to get the data
+
+// there are 3 types of loops
+
+// for loop
+for (let i = 0; i < 8; i++) {
+    parseInt(prompt("Enter the first number, 24"));
+}
+
+// while loop
+
+// do while loop
